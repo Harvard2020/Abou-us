cdd49880d4872be9786e7b9da83d04e86625b4f7
Harvard2020 <65506613+Harvard2020@users.noreply.github.com>
2020-06-09 11:01:48 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,111 +1,61 @@
-if (!location.hash) {
-  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
-}
-const roomHash = location.hash.substring(1);
-  
-const drone = new ScaleDrone('2xmbUiTsqTzukyf7');
-// Room name needs to be prefixed with 'observable-'
-const roomName = 'observable-' + roomHash;
-const configuration = {
-  iceServers: [{
-    urls: 'stun:stun.l.google.com:19302'
-  }]
+      var peerConn= new RTCPeerConnection();
+peerConn.onaddstream = function (evt) {
+  var videoElem = document.createElement("video");
+  document.appendChild(videoElem);
+  videoElem.src = URL.createObjectURL(evt.stream);
 };
-let room;
-let pc;
-   
-function onSuccess() {};
-function onError(error) {
-  console.error(error);
-};
-  
-drone.on('open', error => {
-  if (error) {
-    return console.error(error);
-  }
-  room = drone.subscribe(roomName);
-  room.on('open', error => {
-    if (error) {
-      onError(error);
-    }
-  });
-  // We're connected to the room and received an array of 'members'
-  // connected to the room (including us). Signaling server is ready.
-  room.on('members', members => {
-    console.log('MEMBERS', members);
-    // If we are the second user to connect to the room we will be creating the offer
-    const isOfferer = members.length === 2;
-    startWebRTC(isOfferer);
-  });
+ navigator.getUserMedia({video: true}, function(stream) {
+  videoElem.src = URL.createObjectURL(stream);
+  peerConn.addStream(stream);
+
+  peerConn.createOffer(function(offer) {
+    peerConn.setLocalDescription(new RTCSessionDescription(offer), function() {
+      // send the offer to a server to be forwarded to the other peer
+    }, error);
+  }, error);
 });
-  
-function sendMessage(message) {
-  drone.publish({
-    room: roomName,
-    message
-  });
-}
-  
-function startWebRTC(isOfferer) {
-  pc = new RTCPeerConnection(configuration);
-  
-  pc.onicecandidate = event => {
-    if (event.candidate) {
-      sendMessage({'candidate': event.candidate});
-    }
-  };
-  
-  if (isOfferer) {
-    pc.onnegotiationneeded = () => {
-      pc.createOffer().then(localDescCreated).catch(onError);
-    }
-  }
-  
-  pc.onaddstream = event => {
-    remoteVideo.srcObject = event.stream;
-  };
-  
-  navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  }).then(stream => {
-    // Display your local video in #localVideo element
-    localVideo.srcObject = stream;
-    // Add your stream to be sent to the conneting peer
-    pc.addStream(stream);
-  }, onError);
-  
-  // Listen to signaling data from Scaledrone
-  room.on('data', (message, client) => {
-    // Message was sent by us
-    if (client.id === drone.clientId) {
-      return;
-    }
-  
-    if (message.sdp) {
-      // This is called after receiving an offer or answer from another peer
-      pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-        // When receiving an offer lets answer it
-        if (pc.remoteDescription.type === 'offer') {
-          pc.createAnswer().then(localDescCreated).catch(onError);
-        }
-      }, onError);
-    } else if (message.candidate) {
-      // Add the new ICE candidate to our connections remote description
-      pc.addIceCandidate(
-        new RTCIceCandidate(message.candidate), onSuccess, onError
-      );
-    }
-  });
-}
-  
-function localDescCreated(desc) {
-  pc.setLocalDescription(
-    desc,
-    () => sendMessage({'sdp': pc.localDescription}),
-    onError
-  );
-}
+navigator.getUserMedia({video: true}, function(stream) {
+  videoElem.src = URL.createObjectURL(stream);
+  peerConn.addStream(stream);
+
+  peerConn.setRemoteDescription(new RTCSessionDescription(offer), function() {
+    peerConn.createAnswer(function(answer) {
+      peerConn.setLocalDescription(new RTCSessionDescription(answer), function() {
+        // send the answer to a server to be forwarded back to the caller
+      }, error);
+    }, error);
+  }, error);
+});
+var peerConnCfg =  {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]},
+    peerConn= new RTCPeerConnection(peerConnCfg),
+    signalingChannel = new WebSocket('ws://my-websocket-server:port/');
+
+peerConn.onicecandidate = function (evt) {
+  // send any ice candidates to the other peer, i.e., evt.candidate
+  signalingChannel.send(JSON.stringify({ "candidate": evt.candidate }));
+};
+
+signalingChannel.onmessage = function (evt) {
+  var signal = JSON.parse(evt.data);
+  if (signal.sdp)
+    peerConn.setRemoteDescription(new RTCSessionDescription(signal.sdp));
+  else if (signal.candidate)
+    peerConn.addIceCandidate(new RTCIceCandidate(signal.candidate));
+};
+  var peerConn= new RTCPeerConnection(),
+    dc = peerConn.createDataChannel("my channel");
+
+dc.onmessage = function (event) {
+  console.log("received: " + event.data);
+};
+
+dc.onopen = function () {
+  console.log("datachannel open");
+};
+
+dc.onclose = function () {
+  console.log("datachannel close");
+};
 <video id=”localvid” autoplay </video>
 <button type = “button” onclick=”startvideo ( );” >start video </button>
 <button type = “button” onclick=”stoptvideo ( );” >stop video </button>
