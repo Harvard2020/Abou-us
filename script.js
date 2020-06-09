630d80ac1b0298b06f05aef0eb099d23e960e601
Harvard2020 <65506613+Harvard2020@users.noreply.github.com>
2020-06-09 19:27:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-/** browser dependent definition are aligned to one and the same standard name **/
-navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
-window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition 
-  || window.msSpeechRecognition || window.oSpeechRecognition;
+
 
 const WebSocketServer = require('ws').Server,
   express = require('express'),
