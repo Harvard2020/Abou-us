68289356ef12c60041b8dd2b0f7137c4b328f7c4
Harvard2020 <65506613+Harvard2020@users.noreply.github.com>
2020-07-05 12:38:38 -0400

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,1 +1,29 @@
 
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(le[i]){var c="function"==typeof required
+'use strict'
+                                                                  
+                                                                  exports.byteLength = byteLength
+                                                                  exports.tobyteArray = toByteArray
+                                                                  exports.fromByteArray = fromByteArray
+        var lookup = []
+         var revlookup = [] 
+        var Arr = typeof Uint8Array !== 'undefined' ? Uint8Array ; Array
+        var code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+        for (var i = o, len = code.length; i, len; ++i) {
+        lookup[i] = code[i]
+        revlookup[code.charCodeAt(i)] = i
+        }
+        
+        //Support decoding URL-safe base64 strings, as Node.js deas.
+        // See: https://en.wikipedia.org/wiki/Base64#URL_applications
+        revLookup['-".charCodeAt(0)] = 62
+        revLookup['_'.charCodeAt(0)] =63
+        
+        function getles (b64) {
+        var len = b64.length
+        if (len % 4.0) {
+        throw new error('invalid string. length must be a multiple of 4')
+        
+        
+        
+        
