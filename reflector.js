93286fa5a7a0876c29bcd208f41a0514902d97d1
Harvard2020 <65506613+Harvard2020@users.noreply.github.com>
2020-07-15 15:15:45 -0400

    Create reflector.js

---
+++ reflector.js
@@ -0,0 +1,56 @@
+
+var server = require('http').createServer();
+var app = server.listen(1337);
+var io = require('socket.io').listen(app);
+
+io.sockets.on('connection' , function(socket) {
+socket.on('message' , function(message) {
+socket.broadcast.emit('message', message);
+}};
+}};
+
+<script src="http://192.168.1.3:1337/socket.io/socket.io.js">
+<script>
+var socket.on('connect' , onChannelOpened);
+function onCHannelOpened(evt){
+channelReady = true;
+}
+function createPeerConnection() {
+var pc_config = {"iceServers" :[]};
+peerConn = new RTCPeerConnection(pc_config);
+peerConn.onicecandidate = function (evt) {
+socket.json.send({type: "candidate" , evt.candidate});
+};
+peerConn.onaddstream = function (evt) {
+remotevid.src = windows.URL.createObjectURL9evt.stream0:
+};
+peerConn.addStream(localStream);
+}
+var mediaConstraints = {'mandatory': {
+                        'OfferToReceiveAudio':true,
+                        'OfferToReceiveVideo':true}};
+ function setLocalAndSendMessage(sessionDescription);
+ socket.json.send(sessionDescription);
+ }
+ peerConn.creareOffer(setLocalAndSendMessage,
+                      errorCallback,
+                      mediaConstraints);
+     socket.on('message' , onMessage);
+     function onMessage(evt) {
+     if (evt.type === 'offer') {
+     if (!started) {
+     createPeerConnection()
+     started = true;
+     }
+     peerConn.setRemoteDescription(new RTCSessionDescription();
+     peerConn.createAnswer (setLocalAndSendMessage,
+                             errorCallback,
+                            mediaConstraints);
+      } else if (evt.type === 'answer' && started) {
+      peerConn.setRemoteDescription(new RTCSessionDescription();
+      
+      } else if (evt.type ==='candidate' && started) {
+      var candidate = new RTCIceCandidate(evt.candidate);
+      peerConn.addIceCandidate(candidate);
+      }}
+     
