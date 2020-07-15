ebdd479a7235cba2732eb91528c7907cbcbb2c77
Harvard2020 <65506613+Harvard2020@users.noreply.github.com>
2020-07-14 22:06:18 -0400

    Update video.js

--- video.js
+++ video.js
@@ -1,12 +1,4 @@
-// Determine the room name and public URL for this chat session.
-function getRoom() {
-  var query = location.search && location.search.split("?")[1];
 
-  if (query) {
-    return (location.search && decodeURIComponent(query.split("=")[1]));
-  }
-
-}
 
 // Retrieve the absolute room URL.
 function getRoomURL() {
