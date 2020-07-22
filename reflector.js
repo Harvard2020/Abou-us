ad916eea1814549e369a00a602d726ba95d3d4f5
Harvard2020 <65506613+Harvard2020@users.noreply.github.com>
2020-07-22 16:41:07 -0400

    Update reflector.js

--- reflector.js
+++ reflector.js
@@ -5,9 +5,10 @@ var app = server.listen(1337);
 var io = require('socket.io').listen(app);
 
 io.sockets.on('connection' , function(socket) {
-socket.on('message' , function(message) {
+socket.on ('message' , function(message) {
 socket.broadcast.emit('message', message);          
-       }        
+       }
+           )
 }
           );
 
